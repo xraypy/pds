d7f0ce54825f5776a4c8f6502cd3876d81209e7d
Christofanis Skordas <55315750+skordaschristofanis@users.noreply.github.com>
2025-07-10 11:23:06 -0500

    converts active area calculations module with functions for beam, sample, and detector area overlap

--- pds/utils/active_area.py
+++ pds/utils/active_area.py
@@ -11,16 +11,14 @@ arbirary set of pixels (e.g. roi) on image detector... especially if
 shifted off center (or better off center relative to a specific hkl)
 """
 
-import numpy as num
+import numpy as np
 from matplotlib import pyplot
 
-from mathutil import cartesian_mag, cartesian_angle
-
-from polygon import poly_area, poly_area_num, inner_polygon
-from polygon import plot_polygon, plot_points, plot_circle
+from pds.utils.mathutil import cartesian_angle, cartesian_mag
+from pds.utils.polygon import inner_polygon, plot_circle, plot_points, plot_polygon, poly_area, poly_area_num
 
 
-def active_area(nm, ki=num.array([0.0, 1.0, 0.0]), kr=num.array([0.0, 1.0, 0.0]), beam=[], det=None, sample=1.0, plot=False, fig=None):
+def active_area(nm, ki=np.array([0.0, 1.0, 0.0]), kr=np.array([0.0, 1.0, 0.0]), beam=[], det=None, sample=1.0, plot=False, fig=None):
     """
     Calc the area of overlap of beam, sample and detector
     surface polygon projections.
@@ -66,8 +64,8 @@ def active_area(nm, ki=num.array([0.0, 1.0, 0.0]), kr=num.array([0.0, 1.0, 0.0])
     # calc k vectors in the surf frame (i.e. [xs,ys,zs]) first make them unit vectors, even though no real need to..
     ki = ki / cartesian_mag(ki)
     kr = kr / cartesian_mag(kr)
-    ki_s = num.dot(M, ki)
-    kr_s = num.dot(M, kr)
+    ki_s = np.dot(M, ki)
+    kr_s = np.dot(M, kr)
 
     # Get the beam, detector and surface polygons.
     # Convert vectors to surface frame.
@@ -78,38 +76,38 @@ def active_area(nm, ki=num.array([0.0, 1.0, 0.0]), kr=num.array([0.0, 1.0, 0.0])
     # ie in-plane [x_s, y_s] points
 
     # beam
-    if type(beam) == list:
+    if type(beam) is list:
         beam_poly = []
         if len(beam) < 3:
             print("Error in beam description")
             return None
         for v in beam:
-            vs = num.dot(M, v)
+            vs = np.dot(M, v)
             int = surface_intercept(ki_s, vs)
             beam_poly.append(int)
     else:
         beam_poly = None
     # det
-    if type(det) == list:
+    if type(det) is list:
         det_poly = []
         if len(det) < 3:
             print("Error in det description")
             return None
         for v in det:
-            vs = num.dot(M, v)
+            vs = np.dot(M, v)
             int = surface_intercept(kr_s, vs)
             det_poly.append(int)
     else:
         det_poly = None
     # sample
-    if type(sample) == list:
+    if type(sample) is list:
         sam_poly = []
         if len(sample) < 3:
             print("Error in sample description")
             return None
         for v in sample:
-            vs = num.dot(M, v)
-            if num.fabs(vs[2]) > 0.01:
+            vs = np.dot(M, v)
+            if np.fabs(vs[2]) > 0.01:
                 print("Warning sample hieght problem")
             sam_poly.append(vs[:2])
         sample_shape = True
@@ -119,7 +117,7 @@ def active_area(nm, ki=num.array([0.0, 1.0, 0.0]), kr=num.array([0.0, 1.0, 0.0])
 
     # depending on whether we have a sample shape description or assume a round sample, pass along appropriate
     # data to compute areas.  Note all the polygons are lists of 2D surface frame vectors (ie in plane vectors)
-    if sample_shape == False:
+    if not sample_shape:
         (A_beam, A_int) = _area_round(beam_poly, det_poly, diameter=sample, plot=plot, fig=fig)
     else:
         (A_beam, A_int) = _area_polygon(beam_poly, det_poly, sam_poly, plot=plot, fig=fig)
@@ -138,17 +136,17 @@ def _area_round(beam_poly, det_poly, diameter=None, plot=False, fig=None):
       sample overlap ie A_int/A_beam = spill fraction
     """
     if plot:
-        if fig != None:
+        if fig is not None:
             pyplot.figure(fig)
         pyplot.clf()
     A_beam = poly_area(beam_poly)
-    if det_poly != None:
+    if det_poly is not None:
         inner_poly = inner_polygon(beam_poly, det_poly)
     else:
         inner_poly = beam_poly
     if diameter <= 0.0:
         diameter = None
-    if diameter == None:
+    if diameter is None:
         A_int = poly_area(inner_poly)
     else:
         A_int = poly_area_num(beam_poly, diameter=diameter, plot=plot)
@@ -156,15 +154,15 @@ def _area_round(beam_poly, det_poly, diameter=None, plot=False, fig=None):
     # make plots
     if plot:
         plot_polygon(beam_poly, fmt="ro-", label="Beam")
-        if det_poly != None:
+        if det_poly is not None:
             plot_polygon(det_poly, fmt="ko-", label="Detector")
             plot_points(inner_poly, fmt="go-")
             plot_polygon(inner_poly, fmt="g--", linewidth=4, label="Intersection")
-        if diameter != None:
+        if diameter is not None:
             plot_circle(diameter / 2.0, fmt="b-", label="Sample")
         pyplot.xlabel("Xs")
         pyplot.ylabel("Ys")
-        ll = round(num.sqrt(100.0 * A_int))
+        ll = round(np.sqrt(100.0 * A_int))
         pyplot.xlim(-ll, ll)
         pyplot.ylim(-ll, ll)
         pyplot.grid()
@@ -185,30 +183,30 @@ def _area_polygon(beam_poly, det_poly, sam_poly, plot=False, fig=None):
       sample overlap ie A_int/A_beam = spill fraction
     """
     if plot:
-        if fig != None:
+        if fig is not None:
             pyplot.figure(fig)
         pyplot.clf()
     A_beam = poly_area(beam_poly)
-    if sam_poly == None:
+    if sam_poly is not None:
         inner_poly = beam_poly
     else:
         inner_poly = inner_polygon(beam_poly, sam_poly)
-    if det_poly != None:
+    if det_poly is not None:
         inner_poly = inner_polygon(inner_poly, det_poly)
     A_int = poly_area(inner_poly)
 
     # make plots
     if plot:
         plot_polygon(beam_poly, fmt="ro-", label="Beam")
-        if sam_poly != None:
+        if sam_poly is not None:
             plot_polygon(sam_poly, fmt="bo-", label="Sample")
-        if det_poly != None:
+        if det_poly is not None:
             plot_polygon(det_poly, fmt="ko-", label="Detector")
         plot_points(inner_poly, fmt="go-")
         plot_polygon(inner_poly, fmt="g--", linewidth=4, label="Intersection")
         pyplot.xlabel("Xs")
         pyplot.ylabel("Ys")
-        ll = round(num.sqrt(100.0 * A_int))
+        ll = round(np.sqrt(100.0 * A_int))
         pyplot.xlim(-ll, ll)
         pyplot.ylim(-ll, ll)
         pyplot.grid()
@@ -253,15 +251,15 @@ def calc_surf_transform(nm):
     """
     v_z = nm / cartesian_mag(nm)
 
-    v = num.array([0.0, -1.0, 0.0])
-    v_y = v - (num.dot(v, v_z) * v_z) / (cartesian_mag(v_z) ** 2.0)
+    v = np.array([0.0, -1.0, 0.0])
+    v_y = v - (np.dot(v, v_z) * v_z) / (cartesian_mag(v_z) ** 2.0)
     v_y = v_y / cartesian_mag(v_y)
 
-    v_x = num.cross(v_y, v_z)
+    v_x = np.cross(v_y, v_z)
     v_x = v_x / cartesian_mag(v_x)
 
-    F = num.array([v_x, v_y, v_z])
-    M = num.linalg.inv(F.transpose())
+    F = np.array([v_x, v_y, v_z])
+    M = np.linalg.inv(F.transpose())
 
     return M
 
@@ -278,7 +276,7 @@ def surface_intercept(k, v):
     x_int = v[0] - (k[0] / k[2]) * v[2]
     y_int = v[1] - (k[1] / k[2]) * v[2]
 
-    vi = num.array([x_int, y_int])
+    vi = np.array([x_int, y_int])
 
     return vi
 
@@ -304,21 +302,21 @@ def surface_intercept_bounds(k, v, diameter):
     #    vi_new = vi - ks*mag/norm(ks),
     #    mag(vi_new) = diameter/2
     # therefore end up with a quadratic:
-    ks = num.array(k[0:2])
+    ks = np.array(k[0:2])
     a = 1.0
     b = -2.0 * (v[0] * ks[0] + vi[1] * ks[1]) / cartesian_mag(ks)
     c = cartesian_mag(vi) ** 2.0 - (diameter / 2.0) ** 2.0
     arg = b**2.0 - 4.0 * a * c
     # make sure this method will result in a solution
     if arg > 0.0:
-        mag1 = (-b + num.sqrt(arg)) / (2.0 * a)
-        mag2 = (-b - num.sqrt(arg)) / (2.0 * a)
+        mag1 = (-b + np.sqrt(arg)) / (2.0 * a)
+        mag2 = (-b - np.sqrt(arg)) / (2.0 * a)
         vi1 = vi - ks * mag1 / cartesian_mag(ks)
         vi2 = vi - ks * mag2 / cartesian_mag(ks)
         # chose soln that makes smallest angle
         # with the original intercept
-        angle1 = num.fabs(cartesian_angle(vi, vi1))
-        angle2 = num.fabs(cartesian_angle(vi, vi2))
+        angle1 = np.fabs(cartesian_angle(vi, vi1))
+        angle2 = np.fabs(cartesian_angle(vi, vi2))
         if angle1 < angle2:
             return vi1
         else:
@@ -356,7 +354,6 @@ def test2():
     # sample = 1.5
     # compute active_area
     print(active_area(psic.nm, ki=psic.ki, kr=psic.kr, beam=beam, det=det, sample=sample, plot=True))
-
 
 
 if __name__ == "__main__":
