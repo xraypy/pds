9d7d947876611649aec2ee70b82f6452284182ac
Christofanis Skordas <skordasc@uchicago.edu>
2026-06-30 16:07:20 -0500

    adds Energy (keV) x-axis label for Escan rod plots

--- pds/gui/integrator.py
+++ pds/gui/integrator.py
@@ -1781,6 +1781,8 @@ class Integrator(wx.Frame, wx.Notebook):
                 rodPlot.semilogy()
         except Exception:
             pass
+        if bytes_to_str(self.hdfObject[itemData]["type"]).startswith("Escan"):
+            rodPlot.set_xlabel("Energy (keV)")
         doneLList.extend(pendingLList)
         doneFList.extend(pendingFList)
         if doneLList:
